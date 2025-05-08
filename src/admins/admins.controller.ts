66f04c56b5058dd6d2d8f543def4ca5b7d672f69
Amir Mabood <amir_mabood@yahoo.com>
2025-05-08 19:50:49 +0300

    ADD : admin.controller - add new admin by admin or superadmin

--- src/admins/admins.controller.ts
+++ src/admins/admins.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   UseGuards,
   Req,
+  BadRequestException,
 } from '@nestjs/common';
 import { AdminsService } from './admins.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
@@ -16,7 +17,18 @@ import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { Request } from 'express';
 import { AdminAccessGuard, Roles } from 'src/auth/guards/admin-access.guard';
 import { AdminTypeEnum } from './enum/admin-type.enum';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AdminEntity } from './entities/admin.entity';
 
+@ApiTags('Admins')
+@ApiBearerAuth()
 @Controller('admins')
 @UseGuards(AuthGuard, AdminAccessGuard)
 export class AdminsController {
@@ -28,18 +40,66 @@ export class AdminsController {
     return request['user'];
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.adminsService.findOne(+id);
-  }
+  @ApiOkResponse({
+    type: AdminEntity,
+    example: {
+      phone: '09124323435',
+      type: 'employee',
+      name: null,
+      lastName: null,
+      id: 5,
+      isArchive: false,
+      createdAt: '2025-05-08T16:21:06.730Z',
+      updatedAt: '2025-05-08T16:21:06.730Z',
+    },
+  })
+  @ApiBadRequestResponse({
+    example: [
+      {
+        message: 'admin with phone 09124323435 already exists',
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+      {
+        message: 'admin is not allowed to create admin',
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+      {
+        message: 'admin type just could be employee or admin',
+        error: 'Bad Request',
+        statusCode: 400,
+      },
+    ],
+  })
+  @ApiBody({
+    type: CreateAdminDto,
+  })
+  @Roles(AdminTypeEnum.SUPERADMIN, AdminTypeEnum.ADMIN)
+  @Post('add-admin')
+  async createAdmin(
+    @Req() request: Request,
+    @Body() payload: CreateAdminDto,
+  ): Promise<AdminEntity> {
+    const { phone, type } = payload;
+    const creatorAdminType = request['user']['type'];
+    const admin = await this.adminsService.findByPhone(phone);
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
-    return this.adminsService.update(+id, updateAdminDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.adminsService.remove(+id);
+    if (admin)
+      throw new BadRequestException(`admin with phone ${phone} already exists`);
+    if (type === AdminTypeEnum.SUPERADMIN || type === AdminTypeEnum.ALL_ROLES) {
+      throw new BadRequestException(
+        `admin type just could be ${AdminTypeEnum.EMPLOYEE} or ${AdminTypeEnum.ADMIN}`,
+      );
+    }
+    if (
+      creatorAdminType !== AdminTypeEnum.SUPERADMIN &&
+      type === AdminTypeEnum.ADMIN
+    ) {
+      throw new BadRequestException(
+        `${creatorAdminType} is not allowed to create ${type}`,
+      );
+    }
+    return await this.adminsService.create(payload);
   }
 }
--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -8,6 +8,7 @@ import {
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { AdminsService } from 'src/admins/admins.service';
 
 // Define a custom decorator to mark routes as public
 export const Public = () => SetMetadata('isPublic', true);
@@ -17,6 +18,7 @@ export class AuthGuard implements CanActivate {
   constructor(
     private readonly reflector: Reflector,
     private readonly jwtService: JwtService,
+    private readonly adminService: AdminsService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -34,7 +36,13 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_ACCESS_TOKEN_SECRET,
       });
-      request['user'] = payload;
+
+      const admin = await this.adminService.findByPhone(payload.phoneNumber);
+      if (!admin || admin.isArchive) {
+        throw new UnauthorizedException();
+      }
+
+      request['user'] = admin;
     } catch (err) {
       throw new UnauthorizedException();
     }
