bea39c55739d091abdd11528fd4b55179e13b997
Amir Mabood <amir_mabood@yahoo.com>
2025-05-09 12:39:50 +0300

    ADD : admin.module - update admin

--- src/admins/dto/update-admin.dto.ts
+++ src/admins/dto/update-admin.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAdminDto } from './create-admin.dto';
+import { OmitType, PickType } from '@nestjs/swagger';
 
-export class UpdateAdminDto extends PartialType(CreateAdminDto) {}
+export class UpdateAdminDetailsDto extends PartialType(
+  OmitType(CreateAdminDto, ['phone'] as const),
+) {}
+
+export class UpdateAdminPhoneNumber extends PickType(CreateAdminDto, [
+  'phone',
+] as const) {}
